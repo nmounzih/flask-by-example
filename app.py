752669720d5159e3de46ed156c975ebd278384d9
Nadia Mounzih <nmounzih@yahoo.com>
2017-07-14 12:27:32 -0400

    Added web scraper, nltk data, adjusted index.html

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import requests
+import operator
+import re
+import nltk
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
@@ -11,9 +18,37 @@ db = SQLAlchemy(app)
 from models import Result
 
 
-@app.route('/')
-def hello():
-    return 'Hello'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == 'POST':
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+        except:
+            errors.append('Unable to get URL')
+            return render_template('index.html', errors=errors)
+        if r:
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            nltk.data.path.append('./nltk/data')
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            nonPunct = re.compile('.*[A-za-z]*.')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_words_count = Counter(raw_words)
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+            results = sorted(no_stop_words_count.items(),
+                             key=operator.itemgetter(1), reverse=True)
+            try:
+                result = Result(url=url, result_all=raw_words_count,
+                                results_no_stop_words=no_stop_words_count)
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append('Unable to add item to database')
+    return render_template('index.html', errors=errors, results=results)
 
 
 @app.route('/<name>')
